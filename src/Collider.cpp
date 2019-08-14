7a15f257519e17d91d6099fde9fdfeeebd485f27
sagar <sagarlama826@gmail.com>
2019-08-15 03:11:17 +0545

    Collider added

---
+++ src/Collider.cpp
@@ -0,0 +1,51 @@
+#include <SFML/Graphics.hpp>
+#include "Collider.hpp"
+namespace Sagar
+{
+		Collider::Collider(sf::Sprite& player):
+				player(player)
+		{
+
+		}
+		bool Collider::CheckCollision(Collider& other, float push)
+		{
+				/* GetPosition is my own function */
+				sf::Vector2f otherPosition = other.GetPosition();
+
+				/* GetHalfSize is my own function */
+				sf::Vector2f otherHalfSize = other.GetHalfSize();
+
+				sf::Vector2f thisPosition = GetPosition();
+				sf::Vector2f thisHalfSize = GetHalfSize();
+
+				float deltaX = otherPosition.x - thisPosition.x;
+				float deltaY = otherPosition.y - thisPosition.y;
+
+				float intersectX = abs(deltaX) - (otherHalfSize.x + thisHalfSize.x);
+				float intersectY = abs(deltaY) - (otherHalfSize.y + thisHalfSize.y);
+
+				if(intersectX < 0.0f && intersectY < 0.0f)
+				{
+						push = std::min(std::max(push, 0.0f),1.0f);
+
+						if(intersectX > intersectY)
+						{
+								if(deltaX > 0.0f)
+								{
+										Move(intersectX * (1.0f - push),0.0f);
+										other.Move(-intersectX * push,0.0f);
+								}
+								else
+								{
+										Move(0.0f,-intersectY * (1.0f - push));
+										other.Move(0.0f,intersectY * push);
+								}
+						}
+
+
+						return true;
+				}
+
+				return false;
+		}
+}
---
+++ src/Collider.hpp
@@ -0,0 +1,16 @@
+#pragma once
+namespace Sagar
+{
+		class Collider{
+				public:
+						Collider(sf::Sprite &player);
+						~Collider(){};
+
+						void Move(float dx,float dy){player.move(dx,dy);}
+						bool CheckCollision(Collider& other, float push);
+						sf::Vector2f GetPosition(){return player.getPosition();}
+						sf::Vector2f GetHalfSize(){return static_cast<sf::Vector2f>(player.getTexture()->getSize());}
+				private:
+						sf::Sprite& player;
+		};
+}
