c6cf263f1dfb15df26b25752740f03be6a2617d5
pakhreen <kisantamang1997@gmail.com>
2019-08-16 23:35:07 +0545

    My Collision detector created

--- src/Collider.cpp
+++ src/Collider.cpp
@@ -90,4 +90,15 @@ namespace Sagar
 				size.y = static_cast<float>(player.getTexture()->getSize().y*0.3f/2.0f);
 				return size;
 		}
+
+
+
+		bool Collider::MyCollisionDetectoer(sf::Sprite sprite1, sf::Sprite sprite2)
+		{
+				sf::Rect<float> rect1 = sprite1.getGlobalBounds();
+				sf::Rect<float> rect2 = sprite2.getGlobalBounds();
+				if(rect1.intersects(rect2))
+						return true;
+				return false;
+		}
 }
